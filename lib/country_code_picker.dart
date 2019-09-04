e41d6f2031022138079003804504e39841ed2109
Rangana Perera <rangana.perera@itelasoft.com>
2019-09-04 13:02:20 +0530

    Exposed method to get the country information of the initial selection

--- lib/country_code_picker.dart
+++ lib/country_code_picker.dart
@@ -9,6 +9,8 @@ export 'country_code.dart';
 
 class CountryCodePicker extends StatefulWidget {
   final ValueChanged<CountryCode> onChanged;
+  //Exposed new method to get the initial information of the country
+  final ValueChanged<CountryCode> getInitialData;
   final String initialSelection;
   final List<String> favorite;
   final TextStyle textStyle;
@@ -33,6 +35,7 @@ class CountryCodePicker extends StatefulWidget {
 
   CountryCodePicker({
     this.onChanged,
+    this.getInitialData,
     this.initialSelection,
     this.favorite = const [],
     this.textStyle,
@@ -117,6 +120,9 @@ class _CountryCodePickerState extends State<CountryCodePicker> {
       selectedItem = elements[0];
     }
 
+    //Change added: get the initial entered country information
+    _getInitialSelectedData(selectedItem);
+
     favoriteElements = elements
         .where((e) =>
             widget.favorite.firstWhere(
@@ -154,6 +160,12 @@ class _CountryCodePickerState extends State<CountryCodePicker> {
   void _publishSelection(CountryCode e) {
     if (widget.onChanged != null) {
       widget.onChanged(e);
+    }
+  }
+
+  void _getInitialSelectedData(CountryCode initialData){
+    if(widget.getInitialData != null){
+      widget.getInitialData(initialData);
     }
   }
 }
