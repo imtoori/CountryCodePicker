feaa8325caa55eba518d26e4ac689aa363b132bc
Mohammad Hadi <mohamadhadibi@gmail.com>
2021-12-06 09:03:46 +0330

    try to fix

--- lib/country_code_picker.dart
+++ lib/country_code_picker.dart
@@ -288,28 +288,31 @@ class CountryCodePickerState extends State<CountryCodePicker> {
         // backgroundColor: widget.backgroundColor ?? Colors.transparent,
         context: context,
         builder: (context) => Center(
-          child: Container(
-            constraints: BoxConstraints(maxHeight: 500, maxWidth: 400),
-            child: Dialog(
-              child: SelectionDialog(
-                elements,
-                favoriteElements,
-                showCountryOnly: widget.showCountryOnly,
-                emptySearchBuilder: widget.emptySearchBuilder,
-                searchDecoration: widget.searchDecoration,
-                searchStyle: widget.searchStyle,
-                textStyle: widget.dialogTextStyle,
-                boxDecoration: widget.boxDecoration,
-                showFlag: widget.showFlagDialog != null
-                    ? widget.showFlagDialog
-                    : widget.showFlag,
-                flagWidth: widget.flagWidth,
-                size: widget.dialogSize,
-                backgroundColor: widget.dialogBackgroundColor,
-                barrierColor: widget.barrierColor,
-                hideSearch: widget.hideSearch,
-                closeIcon: widget.closeIcon,
-                flagDecoration: widget.flagDecoration,
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: Container(
+              constraints: BoxConstraints(maxHeight: 500, maxWidth: 400),
+              child: Dialog(
+                child: SelectionDialog(
+                  elements,
+                  favoriteElements,
+                  showCountryOnly: widget.showCountryOnly,
+                  emptySearchBuilder: widget.emptySearchBuilder,
+                  searchDecoration: widget.searchDecoration,
+                  searchStyle: widget.searchStyle,
+                  textStyle: widget.dialogTextStyle,
+                  boxDecoration: widget.boxDecoration,
+                  showFlag: widget.showFlagDialog != null
+                      ? widget.showFlagDialog
+                      : widget.showFlag,
+                  flagWidth: widget.flagWidth,
+                  size: widget.dialogSize,
+                  backgroundColor: widget.dialogBackgroundColor,
+                  barrierColor: widget.barrierColor,
+                  hideSearch: widget.hideSearch,
+                  closeIcon: widget.closeIcon,
+                  flagDecoration: widget.flagDecoration,
+                ),
               ),
             ),
           ),
